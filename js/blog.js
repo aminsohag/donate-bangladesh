738482c88e07b2d1b3fb7ee121669980651419a8
aminsohag <aminsohag508@gmail.com>
2025-01-17 19:08:20 +0600

    live link

--- js/blog.js
+++ js/blog.js
@@ -1,9 +1,9 @@
 // blog page
-document.getElementById('blog-page-btn').addEventListener('click', function(){
-    window.location.href = "blog.html";
-})
+// document.getElementById('blog-page-btn').addEventListener('click', function(){
+//     window.location.href = "/blog.html";
+// })
 
-// home page
-document.getElementById('blog-page-btn').addEventListener('click', function(){
-    history.back();
-})
+// // home page
+// document.getElementById('home-page-btn').addEventListener('click', function(){
+//     window.location.href = "/index.html";
+// })
